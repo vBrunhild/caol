0e3b9a8932e006eb9ae866d9dccba5f75b6b25fa
vBrunhild <brunomoretti100@gmail.com>
2025-09-23 14:09:42 -0400

    set up flake for migration work

---
+++ migrate/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My dev flake for Python!";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    environment.localBinInPath = true;
+
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.docker
+            pkgs.pyrefly
+            pkgs.ruff
+            pkgs.uv
+          ];
+        };
+      }
+    );
+  };
+}
