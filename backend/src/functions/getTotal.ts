9a17c1ac1bedbc18c0674f07a7697d535cb45190
vBrunhild <brunomoretti100@gmail.com>
2025-09-25 23:27:20 -0400

    feat(back): create monthly total API

--- backend/src/functions/getTotal.ts
+++ backend/src/functions/getTotal.ts
@@ -1,10 +1,7 @@
 import { db } from "backend/src/database";
 
-export function getTotal(baseQuery: string): number {
-  const stmt = db.prepare<[], { total: number }>(`
-    SELECT COUNT(*) AS total
-    ${baseQuery}
-  `);
-
-  return stmt.get()?.total || 0
+export function getTotal(baseQuery: string, params: any[] = []): number {
+  const query = `SELECT COUNT(*) as total FROM (${baseQuery})`;
+  const stmt = db.prepare<any[], { total: number }>(query);
+  return stmt.get(params)?.total || 0
 }
